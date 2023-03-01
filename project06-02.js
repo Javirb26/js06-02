8ff0bfaa6323df12ea64e329fa03f76f09cfd7f6
Javirb26 <112392832+Javirb26@users.noreply.github.com>
2023-03-01 08:01:30 -0500

    Add files via upload
    JavaScript Web for Warriors: Chapter 6 Hands on Project 2

---
+++ project06-02.js
@@ -0,0 +1,24 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 6
+      Project 06-02
+
+      Project to turn a selection list into a selection of hypertext links
+      Author: Javier Ballester
+      Date: 02/28/2023  
+
+      Filename: project06-02.js
+*/
+
+window.addEventListener("load", function() {
+ let allSelect = this.document.querySelectorAll("form#govLinks select");
+    
+    for (let i = 0; i < allSelect.length; i++) {
+        allSelect[i].onchange = function(evt) {
+            let linkURL = evt.target.value;
+            let newWin = window.open(linkURL);
+        }
+    }
+});
+
+
